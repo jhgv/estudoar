81f6a045689079a58c2186c141d4212bfac660a8
jhgv <jhgv@cin.ufpe.br>
2015-05-13 00:56:12 -0300

    agora o login e cadastro estao usando fragments e nao somente activities, os estados dos fragments sao salvos para nao bugar a tela

--- app/src/main/java/estudoar/cin/ufpe/br/estudoar/LoginActivity.java
+++ app/src/main/java/estudoar/cin/ufpe/br/estudoar/LoginActivity.java
@@ -1,38 +1,46 @@
 package estudoar.cin.ufpe.br.estudoar;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.ProgressDialog;
-import android.content.DialogInterface;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
 
-import com.parse.LogInCallback;
-import com.parse.ParseException;
 import com.parse.ParseFacebookUtils;
-import com.parse.ParseUser;
-
-import java.util.Arrays;
-import java.util.Collection;
 
 public class LoginActivity extends Activity {
+
+    protected Fragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        ParseUser currentUser = ParseUser.getCurrentUser();
-        if (currentUser != null) {
-            goToHomePage();
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out);
+        if(savedInstanceState != null){
+            fragment = getFragmentManager().getFragment(savedInstanceState, "fragment");
+        }else{
+            fragment = new LoginFragment();
+            ft.add(R.id.fragment_login_signup, fragment);
+            ft.commit();
         }
+    }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        try {
+            getFragmentManager().putFragment(outState, "fragment", fragment);
+        }catch (Exception e){
+            Log.d("My App", e.getMessage());
+        }
     }
 
     @Override
@@ -63,66 +71,5 @@ public class LoginActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void normalLogin(View view){
-        final Dialog progressDialog = ProgressDialog.show(this, "", "Entrando...", true);
 
-        final EditText loginInput = (EditText) findViewById(R.id.authLogin);
-        final EditText passwordInput = (EditText) findViewById(R.id.authPassword);
-
-        String username = loginInput.getText().toString().trim();
-        String password = passwordInput.getText().toString().trim();
-
-        ParseUser.logInInBackground(username, password, new LogInCallback() {
-            @Override
-            public void done(ParseUser user, ParseException e) {
-                progressDialog.dismiss();
-                if (user != null) {
-                    goToHomePage();
-                } else {
-                    AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
-                    builder.setMessage(e.getMessage());
-                    builder.setTitle("Desculpe!");
-                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.dismiss();
-                        }
-                    });
-                    AlertDialog dialog = builder.create();
-                    dialog.show();
-                }
-            }
-        });
-    }
-
-    public void facebookLogin(View view){
-        final Dialog progressDialog = ProgressDialog.show(this, "", "Entrando...", true);
-        Collection<String> permissions = Arrays.asList("public_profile", "email", "user_hometown");
-
-        ParseFacebookUtils.logInWithReadPermissionsInBackground(this, permissions, new LogInCallback() {
-            @Override
-            public void done(ParseUser user, ParseException err) {
-                progressDialog.dismiss();
-
-                if (user == null) {
-                    Log.d("MyApp", "Login com Facebook falhou");
-                } else if (user.isNew()) {
-                    goToHomePage();
-                } else {
-                    goToHomePage();
-                }
-            }
-        });
-    }
-
-    public void goToHomePage(){
-        Intent i = new Intent(LoginActivity.this, HomePageActivity.class);
-        startActivity(i);
-        finish();//Tira LoginActivity da pilha de activities para o usuário não poder voltar para a tela de login
-    }
-
-    public void goToSignup(View view){
-        Intent i = new Intent(this, RegisterActivity.class);
-        startActivity(i);
-    }
 }
