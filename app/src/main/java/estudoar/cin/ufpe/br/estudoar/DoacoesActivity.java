82c990b799f9f28a660e568e7b814ff973cab838
jhgv <jhgv@cin.ufpe.br>
2015-06-21 21:32:33 -0300

    busca dinamica feita

--- app/src/main/java/estudoar/cin/ufpe/br/estudoar/DoacoesActivity.java
+++ app/src/main/java/estudoar/cin/ufpe/br/estudoar/DoacoesActivity.java
@@ -33,16 +33,7 @@ public class DoacoesActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_materiais, menu);
 
-        // Associate searchable configuration with the SearchView
-//        SearchManager searchManager =
-//                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-//        SearchView searchView =
-//                (SearchView) menu.findItem(R.id.search).getActionView();
-//        searchView.setSearchableInfo(
-//                searchManager.getSearchableInfo(getComponentName()));
-
-
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -56,10 +47,6 @@ public class DoacoesActivity extends ActionBarActivity {
 
         switch (id){
             case R.id.action_settings:
-                break;
-
-            case R.id.search:
-                onSearchRequested();
                 break;
         }
 
