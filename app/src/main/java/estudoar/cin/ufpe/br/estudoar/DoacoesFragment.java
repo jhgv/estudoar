d63fed4cdca1fe5b859375b5d0bef78a4e9cf420
jhgv <jhgv@cin.ufpe.br>
2015-06-24 21:57:18 -0300

    diantancia da busca e em kilometros

--- app/src/main/java/estudoar/cin/ufpe/br/estudoar/DoacoesFragment.java
+++ app/src/main/java/estudoar/cin/ufpe/br/estudoar/DoacoesFragment.java
@@ -362,7 +362,7 @@ public class DoacoesFragment extends Fragment implements AbsListView.OnItemClick
             double longitude = myLocation.getLongitude();
             double latitude = myLocation.getLatitude();
             ParseGeoPoint currentPoint = new ParseGeoPoint(latitude, longitude);
-            query.whereNear("localizacao", currentPoint);
+            query.whereWithinKilometers("localizacao", currentPoint, 10.0);
 
             doQuery(query);
 
